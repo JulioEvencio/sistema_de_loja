5203313052aa4a8038b58a30ed47b7bc73d80133
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-21 20:59:02 -0300

    Arquivos do produto refatorados

---
+++ sistema_de_loja/linux/Versão refatorada/produto.h
@@ -0,0 +1,15 @@
+#ifndef PRODUTO_H_INCLUDED
+#define PRODUTO_H_INCLUDED
+
+#define PRODUTO_NOME 31
+
+struct Produto {
+    int codigo;
+    char nome[PRODUTO_NOME];
+    float preco;
+    float preco_repor;
+    int quantidade;
+};
+typedef struct Produto Produto;
+
+#endif
