f67d60afe955c7f1bf11f6be7cc289cf3a78d898
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-21 23:24:30 -0300

    Funções auxiliares para leitura do stdin e para pausar a tela foram adicionadas

--- sistema_de_loja/linux/versao refatorada/cabecalho.h
+++ sistema_de_loja/linux/versao refatorada/cabecalho.h
@@ -1,6 +1,12 @@
 #ifndef CABECALHO_H_INCLUDED
 #define CABECALHO_H_INCLUDED
 
-//  Code
+#define BUFFER_TAMANHO 51
+#define LIMPAR_TELA "clear || cls"
+
+char buffer[BUFFER_TAMANHO];
+
+void pausar_tela(void);
+float ler_stdin(char *nome, int tamanho);
 
 #endif
--- sistema_de_loja/linux/versao refatorada/funcoes.c
+++ sistema_de_loja/linux/versao refatorada/funcoes.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
 #include "cabecalho.h"
 
-//  Code
+void pausar_tela(void) {
+    char letra;
+
+    printf("Digite enter para continuar...");
+
+    while ((letra = getchar()) != '\n');
+}
+
+float ler_stdin(char *nome, int tamanho) {
+    int i = 0, tem_letra = 0;
+    char letra;
+
+    while ((letra = getchar()) != '\n') {
+        if (letra != '\n' && i < tamanho - 1) {
+            if (letra < '0' || letra > '9') tem_letra = 1;
+            *(nome + i) = letra;
+            i++;
+        }
+    }
+
+    *(nome + i) = '\0';
+
+    if (tem_letra) return -1;
+
+    return atof(nome);
+}
