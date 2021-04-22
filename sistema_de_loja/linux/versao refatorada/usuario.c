6312f8456768f4cf37d0c9b771dde9719f9989a6
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-04-21 22:43:35 -0300

    Arquivos básicos do usuário foram adicionados

---
+++ sistema_de_loja/linux/versao refatorada/usuario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "usuario.h"
+
+int usuario_criar(Usuario **usuario) {
+    //  Code
+}
+
+int usuario_liberar(Usuario **usuario) {
+    // Code
+}
---
+++ sistema_de_loja/linux/versao refatorada/usuario.h
@@ -0,0 +1,20 @@
+#ifndef USUARIO_H_INCLUDED
+#define USUARIO_H_INCLUDED
+
+#define USUARIO_NOME 31
+#define USUSARIO_LOGIN 16
+#define USUSARIO_SENHA 16
+
+struct Usuario {
+    int id;
+    char nome[USUARIO_NOME];
+    char login[USUSARIO_LOGIN];
+    char senha[USUSARIO_SENHA];
+    float capital;
+};
+typedef struct Usuario Usuario;
+
+int usuario_criar(Usuario **usuario);
+int usuario_liberar(Usuario **usuario);
+
+#endif
