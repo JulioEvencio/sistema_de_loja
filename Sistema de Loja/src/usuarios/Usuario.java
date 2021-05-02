471a4c86e4d4d07ab49e9a7ab9e417b9b11daa42
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-05-02 19:40:48 -0300

    Classe do usuário foi adicionada

---
+++ Sistema de Loja/src/usuarios/Usuario.java
@@ -0,0 +1,26 @@
+package usuarios;
+
+public final class Usuario {
+    private final String nome;
+    private final String login;
+    private final String senha;
+    // Vetor de lojas
+
+    public Usuario(String nome, String login, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+}
