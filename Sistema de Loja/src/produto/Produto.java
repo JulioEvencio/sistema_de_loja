44a32a66944b9599248f0636e5ff0903d42796a3
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-05-02 19:45:12 -0300

    Classe do Produto foi adicionada

---
+++ Sistema de Loja/src/produto/Produto.java
@@ -0,0 +1,37 @@
+package produto;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private double preco;
+
+    public Produto(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
