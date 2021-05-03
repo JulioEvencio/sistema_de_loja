18e1cba7975f7605c3d22c0b16b31a919bbeebfd
JulioEvencio <julioevencio@acad.ifma.edu.br>
2021-05-03 15:25:35 -0300

    Validação de login, criar loja e deletar loja foram adicionadas parar o usuário

--- Sistema de Loja/src/usuarios/Usuario.java
+++ Sistema de Loja/src/usuarios/Usuario.java
@@ -1,15 +1,20 @@
 package usuarios;
 
+import loja.Loja;
+import excecoes.CodigoInvalidoException;
+import java.util.HashMap;
+
 public final class Usuario {
     private final String nome;
     private final String login;
     private final String senha;
-    // Vetor de lojas
+    private final HashMap<Integer, Loja> lojas;
 
     public Usuario(String nome, String login, String senha) {
         this.nome = nome;
         this.login = login;
         this.senha = senha;
+        this.lojas = new HashMap<>();
     }
 
     public String getNome() {
@@ -22,5 +27,20 @@ public final class Usuario {
 
     public String getSenha() {
         return senha;
+    }
+
+    public boolean validarLogin(String login, String senha) {
+        if (this.login.equals(login) && this.senha.equals(senha)) return true;
+        else return false;
+    }
+
+    public void criarLoja(int codigo, String nome, double capital) throws CodigoInvalidoException {
+        if (lojas.containsKey(codigo)) throw new CodigoInvalidoException();
+        else lojas.put(codigo, new Loja(codigo, nome, capital));
+    }
+
+    public void deletarLoja(int codigo) throws CodigoInvalidoException {
+        if (!lojas.containsKey(codigo)) throw new CodigoInvalidoException();
+        else lojas.remove(codigo);
     }
 }
